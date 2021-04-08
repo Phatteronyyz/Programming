af004545b1e1b05111f1e51d6a3d5f9a023e0e9e
Phattarawat <pattarawat123.k@gmail.com>
2021-04-08 23:37:26 +0700

    commit -all

--- Beta programming/Floting point.cpp
+++ Beta programming/Floting point.cpp
@@ -1,19 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int q,i,n;
-    long double ans=1;
-    scanf("%d",&q);
-    while(q--){
-        ans=1;
-        scanf("%d",&n);
-        for(i=1;i<=n;i++){
-            ans*=2;
-        }
-        printf("%.0llf\n",ans);
-    }
-
-
-
-    return 0;
+int main (){
+	int q;
+	scanf("%d",&q);
+	while(q--){
+		int n;
+		long double ans=1;
+		scanf("%d",&n);
+		for(int i=0;i<n;i++){
+			ans *= 2;
+		}
+		printf("%.0Lf\n",ans);
+	}
+	return 0;
 }
