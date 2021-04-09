0b7a12552f83bca7954cb87849613b90594b5865
Phattarawat <pattarawat123.k@gmail.com>
2021-04-09 17:54:15 +0700

    commit -all

--- Beta programming/Kemija.cpp
+++ Beta programming/Kemija.cpp
@@ -1,8 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-char a[200],b[200];
+char a[200];
 int main()
 {
+    int len;
+    gets(a);
+    len=strlen(a);
+    for(int i=0;i<len;i++){
+        printf("%c",a[i]);
+        if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'){
+            i+=2;
+        }
+    }
 
 
     return 0;
